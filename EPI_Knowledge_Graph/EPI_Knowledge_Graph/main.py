d512d81679e761e10a1c4e798ed33b2478546eb5
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-11-24 11:19:05 +0100

    moinsen

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/main.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/main.py
@@ -27,6 +27,7 @@ from GraphModel.Graph import Graph
 if __name__ == '__main__':
     # Graph
     graph = Graph()#luca war hier
+    #moinsen
     graph.team_name = "Einer_ist_zu_viel"  # TODO: Geben Sie Ihrem Team einen Namen!
     graph_content = GraphContent(graph)  # TODO: Hier können Sie den Inhalt und Verbindungen ihrer Knoten anlegen.
 

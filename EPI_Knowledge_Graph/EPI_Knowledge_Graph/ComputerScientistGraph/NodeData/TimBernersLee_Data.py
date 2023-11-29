22eedcdf7ccbbd774750f73deed117bd007a861e
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-11-29 19:02:18 +0100

    data/image for Tim Berners-Lee

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TimBernersLee_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TimBernersLee_Data.py
@@ -1,5 +1,20 @@
 TITEL = "Tim Berners-Lee (1955)"
 
-CONTENT = ("")
+CONTENT = ("Sir Tim Berners-Lee ist ein britischer Informatiker, der als Begründer des World Wide Web gilt."
+           "In den späten 1980er Jahren entwickelte er die grundlegenden Konzepte und Technologien, die das moderne Internet ermöglichen, darunter HTTP und HTML."
+           "Durch die Gründung des World Wide Web Consortium (W3C) setzte er sich für die Standardisierung und Offenheit des Webs ein.\n\n"
+           "Übersicht:\n"
+           "1955: Geboren am 08. Juni 1955 in London, England\n"
+           "1976: Abschluss am Queen's College der Oxford Universität\n"
+           "1980: Beratender Ingenieur am europäischen Kernforschungszentrum CERN\n"
+           "1981: Arbeit bei Image Computer Systems, Bournemouth\n"
+           "1984: Rückkehr zu CERN\n"
+           "1989: Entwicklung des Konzepts 'World Wide Web'\n"
+           "1990: Implementierung des ersten Webservers 'CERN httpd' und des ersten Webbrowsers 'WorldWideWeb', sowie die Entwicklung der textbasierten Auszeichnungssprache HTML, des Transferprotokolls HTTP und der URI\n"
+           "1994: Gründung des World Wide Web Consortium (W3C) mit dem Ziel, offene Standards für das World Wide Web festzulegen\n"
+           "      und die konsistente und barrierefreie Funktion von Webseiten und Webanwendungen auf verschiedenen Plattformen sicherzustellen\n"
+           "1999: Erster Inhaber des 3Com-Founder Lehrstuhls am Massachusetts Institute of Technology (MIT)\n"
+           "2013: Ehrung mit dem Queen Elizabeth Preis für Ingenieurwissenschaften\n"
+           "2017: Auszeichnung mit dem Turing Preis für die Erfindung des World Wide Web, des ersten Webbrowsers sowie grundlegender Protokolle und Algorithmen, die die Skalierbarkeit des modernen Internets ermöglichten\n")
 
-IMAGE_NAME = "image_placeholder.png"
+IMAGE_NAME = "Tim_Berners-Lee.png"

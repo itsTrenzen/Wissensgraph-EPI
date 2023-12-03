80395d86222ea6f297ee721d21def3814e38cfc6
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-12-03 15:36:39 +0100

    data/image for alonzo church, restructured project

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Organizations/World_Wide_Web_Consortium_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Organizations/World_Wide_Web_Consortium_Data.py
@@ -1,6 +1,6 @@
-TITEL = "World Wide Web Consortium (W3C)"
+TITEL = "World Wide WebTechnologies Consortium (W3C)"
 
-CONTENT = ("Das World Wide Web Consortium ist ein Gremium mit dem Ziel, die Techniken im World Wide Web zu Standardisieren."
+CONTENT = ("Das World Wide WebTechnologies Consortium ist ein Gremium mit dem Ziel, die Techniken im World Wide WebTechnologies zu Standardisieren."
            "W3C wurde 1994 von Tim Berners-Lee gegründet um die inkonsistente Nutzung der Technologien HTML, HTTP und URI zu verhindern."
             "Heute besteht W3C aus 400 Mitgliedern, 50 Mitarbeitern und über 12.000 mitwirkenden Entwicklern weltweit. Zusammen entwickeln die "
            "Zusammen arbeiten sie daran, \n\n"
--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/Nodes_Init.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/Nodes_Init.py
@@ -5,13 +5,17 @@ from EPI_Knowledge_Graph.EPI_Knowledge_Graph.ComputerScientistGraph.NodeData.Com
     TimBernersLee_Data
 from EPI_Knowledge_Graph.EPI_Knowledge_Graph.ComputerScientistGraph.NodeData.ComputerScientists import \
     AlanTuring_Data
+from EPI_Knowledge_Graph.EPI_Knowledge_Graph.ComputerScientistGraph.NodeData.ComputerScientists import \
+    AlonzoChurch_Data
 
 
 class Nodes:
     def __init__(self, graph: Graph):
         graph.add_new_node_to_graph(TIM_BERNERS_LEE)
         graph.add_new_node_to_graph(ALAN_TURING)
+        graph.add_new_node_to_graph(ALONZO_CHURCH)
 
 
 TIM_BERNERS_LEE = Node(TimBernersLee_Data.CONTENT, TimBernersLee_Data.TITEL, TimBernersLee_Data.IMAGE_NAME)
 ALAN_TURING = Node(AlanTuring_Data.CONTENT, AlanTuring_Data.TITEL, AlanTuring_Data.IMAGE_NAME)
+ALONZO_CHURCH = Node(AlonzoChurch_Data.CONTENT, AlonzoChurch_Data.TITEL, AlonzoChurch_Data.IMAGE_NAME)

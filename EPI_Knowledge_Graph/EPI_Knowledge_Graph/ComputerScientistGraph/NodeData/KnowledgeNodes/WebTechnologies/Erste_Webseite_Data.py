96ab19b336943c705c17fc13995999ede37ddf6c
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-12-06 23:11:54 +0100

    html

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/KnowledgeNodes/WebTechnologies/Erste_Webseite_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/KnowledgeNodes/WebTechnologies/Erste_Webseite_Data.py
@@ -3,6 +3,6 @@ TITEL = "Erste Website"
 CONTENT = ("Die erste Website wurde von Tim Berners-Lee 1990\n\n"
 
            "Quellen:\n"
-           "\n")
+           "http://homepages.cs.ncl.ac.uk/brian.randell/Seminars/92.pdf\n")
 
 IMAGE_NAME = "Turing_Award.jpg"
---
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/KnowledgeNodes/WebTechnologies/HTML_Data.py
@@ -0,0 +1,28 @@
+TITEL = "Hypertext Markup Language (HTML)"
+
+CONTENT = ("HTML ist eine textbasierte Auszeichnungssprache, die vor allem im Kontext des World Wide Webs verwendet "
+           "wird. Die erste HTML-Spezifikation wurde von Tim Berners-Lee im Jahre 1992 veröffentlicht. Ziel war es, "
+           "eine für alle Computer verständliche Sprache für die globale Veröffentlichung von Informationen zu "
+           "schaffen.\n\n"
+           
+           "Aufbau\n"
+           "Dokumenttypdefinition (DTD):  formale Spezifikation, die die Struktur und Syntax von XML-Dokumenten "
+           "definiert\n"
+           "* Dokumenttypdeklarationen: definiert die HTML-Version und referenziert die DTD (ausgenommen HTML5)\n"
+           "* Tags: "
+
+           "Beispiel (HTML5):\n"
+           "<!DOCTYPE html>\"\n"
+           "<HTML>\n"
+           "    <HEAD>\n"
+           "        <TITLE>My first HTML document</TITLE>\n"
+           "    </HEAD>\n"
+           "    <BODY>\n"
+           "        <P>Hello world!\n"
+           "    </BODY>\n"
+           "</HTML>\n\n"
+
+           "Quellen:\n"
+           "http://homepages.cs.ncl.ac.uk/brian.randell/Seminars/92.pdf\n")
+
+IMAGE_NAME = ""

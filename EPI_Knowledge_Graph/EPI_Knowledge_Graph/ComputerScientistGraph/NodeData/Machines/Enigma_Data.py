54410bf9fa86088c5bf97b11459d658a9020c17f
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-12-11 23:14:29 +0100

    enigma data/image

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Machines/Enigma_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Machines/Enigma_Data.py
@@ -1,10 +1,31 @@
-TITEL = "Enigma"
+TITEL = "Militär-Enigma (1930)"
 
-CONTENT = ("\n\n"
+CONTENT = ("Die Enigma-Maschine war eine elektromechanische Chiffriermaschine, die während des Zweiten Weltkriegs von "
+           "der deutschen Wehrmacht für militärische Kommunikation verwendet wurde. Entwickelt von der "
+           "Chiffriermaschinen AG (später: Heimsoeth und Rinke) in den Jahren 1927 bis 1929, spielte die Enigma eine "
+           "zentrale Rolle in der deutschen Verschlüsselungstechnologie.\n\n"
+           
+           "Funktionsweise\n"
+           "Die Enigma-Maschine arbeitete auf der Grundlage von Walzen, einem Reflektor und einem Steckerbrett. Der "
+           "Bediener gab den Klartext auf der Tastatur ein, und durch die Verschaltung der Walzen wurde jeder "
+           "Buchstabe in einen verschlüsselten Buchstaben umgewandelt. Das Steckerbrett tauschte Paare von Buchstaben "
+           "aus und fügte eine zusätzliche Ebene der Komplexität hinzu. Die deutsche Wehrmacht änderte täglich die "
+           "Einstellungen der Walzen, was die Entschlüsselung erschwerte.\n\n"
+           
+           "Geschichtliche Bedeutung\n"
+           "Die Enigma-Maschine wurde 1930 in den Dienst gestellt und wurde während des gesamten Zweiten Weltkriegs "
+           "von der deutschen Wehrmacht eingesetzt. Aufgrund ihrer vermeintlichen Unknackbarkeit galt die von der "
+           "Enigma verschlüsselte Kommunikation als sicher. Die Alliierten knackten die deutschen Codes unter Alan "
+           "Turings Leitung in Bletchley Park, vor allem durch die Turing-Bombe. Die Entschlüsselung der Enigma trug "
+           "wesentlich dazu bei, den Kriegsverlauf zu beeinflussen, da die Alliierten Zugang zu geheimen deutschen "
+           "Nachrichten erhielten. Die Enigma-Maschine repräsentiert einen Meilenstein in der Geschichte "
+           "der Kryptographie und hatte erhebliche Auswirkungen auf den Ausgang des Zweiten Weltkriegs.\n\n"
 
            "Quellen:\n"
-           "\n")
+           "Smart, Nigel P. (2015): The Enigma Machine, in: Information security and cryptography, [online] "
+           "doi:10.1007/978-3-319-21936-3_8.\n"
+           "Enigma i (o. D.): [online] https://www.cryptomuseum.com/crypto/enigma/i/index.htm.\n")
 
-IMAGE_NAME = ""
+IMAGE_NAME = "Enigma.jpg"
 
 CONNECTIONS = ["ALAN_TURING", "BOMBES"]

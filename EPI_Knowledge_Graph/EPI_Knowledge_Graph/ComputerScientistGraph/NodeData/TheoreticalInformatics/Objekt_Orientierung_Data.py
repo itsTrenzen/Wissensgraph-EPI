9c4d80a27e370ae71f7fab0698a4d78f0cd50287
Lumir04 <151911917+Lumir04@users.noreply.github.com>
2023-12-12 20:38:53 +0100

    Objektorientierung

---
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TheoreticalInformatics/Objekt_Orientierung_Data.py
@@ -0,0 +1,38 @@
+TITEL = "Objekt-orientierung"
+CONTENT = ("""**Grundlagen der Objektorientierung:**
+
+- **Paradigma:** Die Objektorientierung (OO) ist ein Programmierparadigma, das auf dem Konzept von "Objekten" basiert.
+  
+- **Objekte:** Objekte sind Instanzen von Klassen und können Daten (Attribute) sowie Methoden (Funktionen) enthalten.
+
+- **Klassen:** Klassen sind Baupläne für Objekte, die Attribute und Methoden spezifizieren.
+
+- **Inkapselung:** Objekte können ihre internen Strukturen vor externen Einflüssen schützen, indem sie Daten und Methoden in Klassen kapseln.
+
+- **Vererbung:** Klassen können von anderen Klassen erben, wodurch gemeinsame Eigenschaften und Verhalten wiederverwendet werden können.
+
+- **Polymorphismus:** Es ermöglicht, dass Objekte unterschiedliche Formen annehmen können. Das gleiche Interface kann von verschiedenen Klassen implementiert werden.
+
+**Geschichte der Objektorientierung:**
+
+- **1950er-1960er Jahre:** Frühe Konzepte der Objektorientierung wurden in der Programmiersprache Simula entwickelt. Simula war darauf ausgerichtet, reale Systeme und ihre Interaktionen zu modellieren.
+
+- **1970er Jahre:** Alan Kay prägte den Begriff "Objektorientierung" und entwickelte die Programmiersprache Smalltalk, die als eine der ersten vollständig objektorientierten Sprachen gilt.
+
+- **1980er Jahre:** Objektorientierung wird in verschiedenen Programmiersprachen integriert, darunter C++, eine Erweiterung von C, und Objective-C. C++ führte Konzepte wie Klassen und Vererbung ein.
+
+- **1990er Jahre:** Java wird veröffentlicht und gewinnt aufgrund seiner plattformunabhängigen Natur und objektorientierten Eigenschaften an Popularität. Auch andere Sprachen wie Python und Ruby erleben einen Aufschwung.
+
+- **2000er Jahre:** Objektorientierung wird weiterhin in vielen modernen Sprachen wie C#, Swift, und Kotlin verwendet. Design Patterns und Frameworks fördern bewährte Praktiken für die Objektorientierung.
+
+- **Heute:** Objektorientierung ist weit verbreitet und bildet die Grundlage für viele moderne Softwareentwicklungspraktiken. Es gibt jedoch auch andere Programmierparadigmen wie funktionale Programmierung, die neben der Objektorientierung existieren.
+
+Die Objektorientierung hat die Art und Weise, wie Software entworfen und entwickelt wird, grundlegend verändert und ist heute ein grundlegendes Konzept in der Softwareentwicklung.""")
+
+"Quellen:http://openbook.rheinwerk-verlag.de/oop/\n"
+"http://openbook.rheinwerk-verlag.de/javainsel/03_001.html\n"
+"http://propella.sakura.ne.jp/earlyHistoryST/EarlyHistoryST.html"
+
+IMAGE_NAME = ""
+
+CONNECTIONS = ["SPRACHE_JAVA"]

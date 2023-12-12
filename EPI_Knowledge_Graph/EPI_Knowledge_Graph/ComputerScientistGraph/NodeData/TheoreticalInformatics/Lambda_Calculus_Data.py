5d29d3c80e0a83187d0455b5050fe688808fa59f
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-12-12 14:42:27 +0100

    bug fixes

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TheoreticalInformatics/Lambda_Calculus_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TheoreticalInformatics/Lambda_Calculus_Data.py
@@ -1,10 +1,28 @@
 TITEL = "Lambda Kalkül"
 
-CONTENT = ("\n\n"
+CONTENT = ("Das Lambda-Kalkül, von Alonzo Church in den 1930er Jahren entwickelt, ist ein fundamentales "
+           "mathematisches Konzept, das dazu dient, den Begriff der Berechenbarkeit auf eine präzise und formale "
+           "Weise zu definieren. Die Grundidee hinter dem Lambda-Kalkül besteht darin, Funktionen als abstrakte "
+           "Entitäten zu betrachten und Berechnungen durch Funktionsanwendung zu modellieren.\n\n"
+
+           "Grundlage für funktionale Programmiersprachen\n"
+           "Eine Anwendungen des Lambda-Kalküls liegt in der funktionalen Programmierung. Funktionale Programmierung "
+           "basiert auf der Idee, dass Funktionen als Basisbausteine für Softwareentwicklung dienen. Der "
+           "Lambda-Kalkül bietet die theoretische Grundlage für diese Programmierparadigmen, indem er zeigt, "
+           "wie Funktionen als Werte behandelt und manipuliert werden können.\n\n"
+
+           "Darstellung im Lambda-Kalkül\n"
+           "Im Lambda-Kalkül wird Abstraktion durch das λ-Symbol dargestellt, gefolgt von einer Variable und einem "
+           "Ausdruck. Zum Beispiel repräsentiert λx.x die Identitätsfunktion (eine mathematische Funktion, die jedem "
+           "Element einer Menge sich selbst zuordnet.\n\n"
 
            "Quellen:\n"
-           "\n")
+           "Dreiseitl, Stephan (2018b): Mathematik für Software Engineering, Springer-Verlag GmbH Deutschland, "
+           "ein Teil von Springer Nature 2018, [eBook] "
+           "https://wwwbroy.in.tum.de/lehre/vorlesungen/perlen/SS09/Unterlagen/lambda.pdf.\n"
+           "Dreiseitl, Stephan (2018): Lambda-Kalkül, in: Springer eBooks, S. 433–449, [online] "
+           "doi:10.1007/978-3-662-56733-3_10.\n")
 
-IMAGE_NAME = ""
+IMAGE_NAME = "Lambda_Calculus.svg"
 
-CONNECTIONS = []
+CONNECTIONS = ["ALONZO_CHURCH"]

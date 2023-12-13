0520a5e2e800fd57c7d33f0fc7d4e613a5d062ee
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2023-12-13 23:23:15 +0100

    harvard citation, church thesis, uri

--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Machines/Computer/UNIVAC_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/Machines/Computer/UNIVAC_Data.py
@@ -5,7 +5,7 @@ CONTENT = ("UNIVAC war der erste kommerziell verfügbare Machines in den Vereini
            "Aufgrund seines Preises und seiner Größe war UNIVAC jedoch nicht für den normalen Bürger zugänglich, "
            "sondern wurde insgesamt nur 46-Mal an verschiedene Institute und Universitäten verkauft.\n\n"
 
-           "Verwendung\n"
+           "### Verwendung\n"
            "Der UNIVAC sollte ursprünglich für die Volkszählung 1951 in den USA eingesetzt werden, wurde aber "
            "aufgrund finanzieller Schwierigkeiten nicht rechtzeitig fertig. Berühmt wurde der UNIVAC später durch "
            "seinen Einsatz bei der Präsidentschaftswahl 1952. Er erstellte eine Hochrechnung basierend auf 7% der "
@@ -13,14 +13,14 @@ CONTENT = ("UNIVAC war der erste kommerziell verfügbare Machines in den Vereini
            "wurde der ENIAC für wissenschaftliche Berechnungen, militärische Zwecke und für die Forschung "
            "eingesetzt.\n\n"
 
-           "Funktionsweise\n"
+           "### Funktionsweise\n"
            "UNIVAC war der erste Machines, der sowohl numerische als auch alphabetische Daten vollständig verarbeiten "
            "konnte. Im Unterschied zu seinem Vorgänger 'ENIAC' setzte er Magnetbänder für sämtliche Ein- und Ausgaben "
            "ein, die später von einem separaten Banddrucker ausgedruckt wurden. Zusätzlich gab es eine Tastatur und "
            "eine Konsole-Schreibmaschine für einfache und begrenzte Eingaben. UNIVAC benutzte ähnlich zu seinen "
            "Vorgängern das Dezimalsystem und konnte 7.200 Dezimalziffern pro Sekunde verarbeiten.\n\n"
 
-           "Technische Daten\n"
+           "### Technische Daten\n"
            "Fläche: 35.5 Quadratmeter\n"
            "Gewicht: 13 Tonnen\n"
            "5.200 Vakuumröhren\n"
@@ -28,7 +28,7 @@ CONTENT = ("UNIVAC war der erste kommerziell verfügbare Machines in den Vereini
            "Kosten: 159.000 US-$ bis 1.500.00 US-$\n"
            "\n\n"
 
-           "Quellen:\n"
+           "### Quellen:\n"
            "Swaine, Michael R./Paul A. Freiberger (2008): UNIVAC | Mainframe Computer, Business Applications & Data "
            "Processing, Encyclopedia Britannica, [online] https://www.britannica.com/technology/UNIVAC.\n"
            "Ralston, Anthony/Edwin D. Reilly/David Hemmendinger (2003): Encyclopedia of Computer Science, John Wiley "
--- EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TheoreticalInformatics/Church_Turing_Thesis_Data.py
+++ EPI_Knowledge_Graph/EPI_Knowledge_Graph/ComputerScientistGraph/NodeData/TheoreticalInformatics/Church_Turing_Thesis_Data.py
@@ -1,10 +1,21 @@
 TITEL = "Church Turing These"
 
-CONTENT = ("\n\n"
+CONTENT = ("Die Church-Turing-These wurde von Alonzo Church und Alan Turing in den 1930er "
+           "Jahren formuliert und lautet: \"Die Klasse der Turing-berechenbaren Funktionen stimmt mit der Klasse der "
+           "intuitiv berechenbaren Funktionen überein.\" . Beide Forscher kamen zu dem Schluss, dass das "
+           "Lambda-Kalkül von Church und die Turing-Maschine gleichermaßen alle intuitiv berechenbaren Funktionen "
+           "repräsentieren können. Diese Erkenntnis legt nahe, dass die Turing-Maschine als universelles "
+           "Berechnungsmodell fungiert und die Church-Turing-These auf alle intuitiv berechenbaren Prozesse anwendbar "
+           "ist. Obwohl die These nicht beweisbar ist, da \"intuitiv berechenbare Funktionen\" nicht klar definiert "
+           "sind, bildet sie die Basis für das Verständnis von algorithmischen Grenzen und universeller "
+           "Berechenbarkeit in der theoretischen Informatik.\n\n"
 
-           "Quellen:\n"
-           "\n")
+           "### Quellen:\n"
+           "The Church-Turing Thesis (Stanford Encyclopedia of Philosophy) (2017): [online] "
+           "https://plato.stanford.edu/entries/church-turing/.\n"
+           "Boker, Udi/Nachum Dershowitz (2022): What is the Church-Turing thesis?, in: Springer eBooks, S. 199–234, "
+           "[online] doi:10.1007/978-3-030-77799-9_9.\n")
 
 IMAGE_NAME = ""
 
-CONNECTIONS = []
+CONNECTIONS = ["CHURCH_THESIS", "ALONZO_CHURCH", "ALAN_TURING", "TURING_MACHINE", "LAMBDA_CALCULUS"]
